87ddf5accc7606cd6ddb1c256e9086c6306d5e0d
Morgan Nance <morganlnance@gmail.com>
2016-01-26 16:29:04 -0800

    starting to try to add parallel processing to the program

--- run_count_contacts.py
+++ run_count_contacts.py
@@ -20,6 +20,7 @@ except ImportError:
     print "Trouble with imports - do you have pandas? Exiting"
     sys.exit()
 import non_rosetta_count_contacts as contact
+import pp
 
 
 
@@ -44,6 +45,15 @@ input_args = parser.parse_args()
 ######################
 #### MAIN PROGRAM ####
 ######################
+
+## try Python parallel processing
+# python server creation
+ppservers = ()
+
+# creates job server with automatically detected number of cores to work with
+job_server = pp.Server( ppservers = ppservers )
+print "Starting pp with", job_server.get_ncpus(), "workers\n"
+
 
 def go( pdb_name_list, ignore_glycosylated_proteins, cutoff, heavy_atoms, download_pdbs, keep_cifs, keep_pdbs, keep_clean_pdbs ):
     # relevant variable instatiations
