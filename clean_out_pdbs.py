c8fdd9b5ea91c45b9d8db784320d151c3f7cd9e4
Morgan Nance <morganlnance@gmail.com>
2016-02-03 10:29:29 -0800

    took out the polar and nonpolar list in clean_out_pdbs because that filter is not used in that program

--- clean_out_pdbs.py
+++ clean_out_pdbs.py
@@ -40,16 +40,14 @@ except ImportError:
 #### CHEMICAL DATA ####
 #######################
 
-# list of polar and nonpolar atoms
-nonpolar_atoms = [ 'C' ]
-polar_atoms = [ 'O', 'N', 'S', 'P', 'F', 'Cl', 'CL', 'Br', 'BR', 'I', 'Se', 'SE', 'B' ]
-
 # list of residues to remove if they are a ligand
 # ligand meaning it was defined as HETATM in the PDB
 metal_list = [ 'B', 'K', 'V', 'Y', 'W', 'U', "LI", "BE", "NA", "MG", "AL", "SI", "CA", "SC", "TI", "CR", "MN", "FE", "CO", "NI", "CU", "ZN", "GA", "GE", "AS", "RB", "SR", "ZR", "NB", "MO", "TC", "RU", "RH", "PD", "AG", "CD", "IN", "SN", "SB", "TE", "CS", "BA", "LU", "HF", "TA", "RE", "OS", "IR", "PT", "AU", "HG", "TI", "PB", "BI", "PO", "AT", "FR", "RA", "LR", "RF", "DB", "SG", "BH", "HS", "MT", "LA", "CE", "PR", "ND", "PM", "SM", "EU", "GD", "TB", "DY", "HO", "ER", "TM", "YB", "AC", "TH", "PA", "NP", "PU", "AM", "CM", "BK", "CF", "ES", "FM", "MD", "NO" ]
 
+# list of the three letter amino acid codes
 AA_list = [ "ALA", "ARG", "ASN", "ASP", "CYS", "GLN", "GLU", "GLY", "HIS", "ILE", "LEU", "LYS", "MET", "PHE", "PRO", "SER", "THR", "TRP", "TRY", "VAL" ]
 
+# list of the codes for the nucleic acids
 nucleic_acid_list = [ 'DA', 'A', 'DT', 'T', 'DG', 'G', 'DC', 'C' ]
 
 
