78c22d837be6044a8769a5c6136cc845fb16e113
mlnance <morganlnance@gmail.com>
2016-02-28 12:30:16 -0800

    moved chemical data into its own file in utility

--- non_rosetta_count_contacts.py
+++ non_rosetta_count_contacts.py
@@ -19,9 +19,6 @@ print "Loading '%s' dependencies...\n" %__name__
 import sys
 import os
 import shutil
-sys.path.append( "utility" )
-from line_definitions import *
-from download_files import download_pdb_file, download_cif_file
 try:
     from colorama import Fore, Style
 except:
@@ -37,23 +34,11 @@ except ImportError:
     print "Trouble with imports - do you have networkx? Exiting"
     sys.exit()
 
-
-
-#######################
-#### CHEMICAL DATA ####
-#######################
-
-# list of polar and nonpolar atoms
-nonpolar_atoms = [ 'C' ]
-polar_atoms = [ 'O', 'N', 'S', 'P', 'F', 'Cl', 'CL', 'Br', 'BR', 'I', 'Se', 'SE', 'B' ]
-
-# list of residues to remove if they are a ligand
-# ligand meaning it was defined as HETATM in the PDB
-metal_list = [ 'B', 'K', 'V', 'Y', 'W', 'U', "LI", "BE", "NA", "MG", "AL", "SI", "CA", "SC", "TI", "CR", "MN", "FE", "CO", "NI", "CU", "ZN", "GA", "GE", "AS", "RB", "SR", "ZR", "NB", "MO", "TC", "RU", "RH", "PD", "AG", "CD", "IN", "SN", "SB", "TE", "CS", "BA", "LU", "HF", "TA", "RE", "OS", "IR", "PT", "AU", "HG", "TI", "PB", "BI", "PO", "AT", "FR", "RA", "LR", "RF", "DB", "SG", "BH", "HS", "MT", "LA", "CE", "PR", "ND", "PM", "SM", "EU", "GD", "TB", "DY", "HO", "ER", "TM", "YB", "AC", "TH", "PA", "NP", "PU", "AM", "CM", "BK", "CF", "ES", "FM", "MD", "NO" ]
-
-AA_list = [ "ALA", "ARG", "ASN", "ASP", "CYS", "GLN", "GLU", "GLY", "HIS", "ILE", "LEU", "LYS", "MET", "PHE", "PRO", "SER", "THR", "TRP", "TRY", "VAL" ]
-
-nucleic_acid_list = [ 'DA', 'A', 'DT', 'T', 'DG', 'G', 'DC', 'C' ]
+# utility functions
+sys.path.append( "utility" )
+from line_definitions import *
+from download_files import download_pdb_file, download_cif_file
+from chemical_data import *
 
 
 
